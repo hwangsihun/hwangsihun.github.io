ed363e9e2a4295d43624d1ceb2a8d0c3a6a77704
황시훈 <hsh2000@virtualdream.co.kr>
2026-04-15 13:48:46 +0900

    init

--- .history/src/main_20260403092813.js
+++
@@ -1,7 +0,0 @@
-import './style/global.css';
-import './style/layout.css';
-import './style/swiper-custom.css';
-import './components/common/buttons/pagination/index.js';
-import './pages/home.js';
-import './components/calendar/index.js';
-import './components/gallery/index.js';
--- .history/src/main_20260403103902.js
+++
@@ -1,8 +0,0 @@
-import './style/global.css';
-import './style/layout.css';
-import './style/responsive.css';
-import './style/swiper-custom.css';
-import './components/common/buttons/pagination/index.js';
-import './pages/home.js';
-import './components/calendar/index.js';
-import './components/gallery/index.js';
